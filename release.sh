9adf31b898a218255c1975c4e94952a65c5fb530
k33g <ph.charriere@gmail.com>
2025-09-15 16:57:16 +0200

    📦 📝 MCP RAG Server v0.0.0: initialize

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+: <<'COMMENT'
+# Relase script for mcp-rag-server
+Usage:
+1. Update the version in release.env
+2. Run this script: ./release.sh
+3. Create a GitHub release with the new tag and description
+COMMENT
+
+set -o allexport; source release.env; set +o allexport
+
+echo "Generating release: ${TAG} ${ABOUT}"
+
+find . -name '.DS_Store' -type f -delete
+
+echo "📝 Replacing ${PREVIOUS_DOCKER_TAG} by ${DOCKER_TAG} in files..."
+
+go run ./release/release.go -old="${PREVIOUS_DOCKER_TAG}" -new="${DOCKER_TAG}" -file="tests/start.with.docker/compose.yml"
+go run ./release/release.go -old="${PREVIOUS_DOCKER_TAG}" -new="${DOCKER_TAG}" -file="README.md"
+
+git add .
+git commit -m "📦 ${ABOUT}"
+git push origin main
+
+git tag -a ${TAG} -m "${ABOUT}"
+git push origin ${TAG}
+
